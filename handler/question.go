7963d530883bd56c6d4ef45e20ebe3b18a319194
hayatroid <hayatroid@gmail.com>
2023-12-11 16:52:12 +0900

    GetQuestionsByLimitAndOffset の実装

--- handler/question.go
+++ handler/question.go
@@ -36,3 +36,33 @@ func (h Handler) GetAllQuestions(c echo.Context) error {
 	}
 	return c.JSON(http.StatusOK, response)
 }
+
+type GetQuestionsByLimitAndOffsetResponce struct {
+	ID        string       `json:"id"`
+	UserID    string       `json:"user_id"`
+	Title     string       `json:"title"`
+	Content   string       `json:"content"`
+	CreatedAt string       `json:"created_at"`
+	Tags      []domain.Tag `json:"tags"`
+	Status    string       `json:"status"`
+}
+
+func (h Handler) GetQuestionsByLimitAndOffset(c echo.Context, limit int, offset int) error {
+	questions, err := h.qrepo.FindByLimitAndOffset(limit, offset)
+	if err != nil {
+		return err
+	}
+	response := make([]GetAllQuestionsResponse, len(questions))
+	for i, q := range questions {
+		response[i] = GetAllQuestionsResponse{
+			ID:        q.ID,
+			UserID:    q.UserID,
+			Title:     q.Title,
+			Content:   q.Content,
+			CreatedAt: q.CreatedAt.String(),
+			Tags:      q.Tags,
+			Status:    string(q.Status),
+		}
+	}
+	return c.JSON(http.StatusOK, response)
+}
