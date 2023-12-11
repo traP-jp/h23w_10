72d8507870f9a45c08ec724392b5a84c9ae10c92
hayatroid <hayatroid@gmail.com>
2023-12-11 17:09:26 +0900

    FindAll の削除，Find 関数の名前の変更

--- handler/question.go
+++ handler/question.go
@@ -7,7 +7,7 @@ import (
 	"github.com/traP-jp/h23w_10/pkg/domain"
 )
 
-type GetAllQuestionsResponse struct {
+type GetQuestionsResponce struct {
 	ID        string       `json:"id"`
 	UserID    string       `json:"user_id"`
 	Title     string       `json:"title"`
@@ -17,44 +17,14 @@ type GetAllQuestionsResponse struct {
 	Status    string       `json:"status"`
 }
 
-func (h Handler) GetAllQuestions(c echo.Context) error {
-	questions, err := h.qrepo.FindAll()
+func (h Handler) GetQuestions(c echo.Context, limit, offset int) error {
+	questions, err := h.qrepo.Find(limit, offset)
 	if err != nil {
 		return err
 	}
-	response := make([]GetAllQuestionsResponse, len(questions))
+	response := make([]GetQuestionsResponce, len(questions))
 	for i, q := range questions {
-		response[i] = GetAllQuestionsResponse{
-			ID:        q.ID,
-			UserID:    q.UserID,
-			Title:     q.Title,
-			Content:   q.Content,
-			CreatedAt: q.CreatedAt.String(),
-			Tags:      q.Tags,
-			Status:    string(q.Status),
-		}
-	}
-	return c.JSON(http.StatusOK, response)
-}
-
-type GetQuestionsByLimitAndOffsetResponce struct {
-	ID        string       `json:"id"`
-	UserID    string       `json:"user_id"`
-	Title     string       `json:"title"`
-	Content   string       `json:"content"`
-	CreatedAt string       `json:"created_at"`
-	Tags      []domain.Tag `json:"tags"`
-	Status    string       `json:"status"`
-}
-
-func (h Handler) GetQuestionsByLimitAndOffset(c echo.Context, limit int, offset int) error {
-	questions, err := h.qrepo.FindByLimitAndOffset(limit, offset)
-	if err != nil {
-		return err
-	}
-	response := make([]GetAllQuestionsResponse, len(questions))
-	for i, q := range questions {
-		response[i] = GetAllQuestionsResponse{
+		response[i] = GetQuestionsResponce{
 			ID:        q.ID,
 			UserID:    q.UserID,
 			Title:     q.Title,
--- pkg/domain/repository/question.go
+++ pkg/domain/repository/question.go
@@ -3,8 +3,7 @@ package repository
 import "github.com/traP-jp/h23w_10/pkg/domain"
 
 type QuestionRepository interface {
-	FindAll() ([]domain.Question, error)
-	FindByLimitAndOffset(limit int, offset int) ([]domain.Question, error)
+	Find(limit, offset int) ([]domain.Question, error)
 	FindByID(id string) (*domain.Question, error)
 	Create(question *domain.Question) (*domain.Question, error)
 }
