d9331badb63c34569060f7260e94aec516ffdec3
pirosiki197 <lingmukangtai825@gmail.com>
2023-12-12 13:12:05 +0900

    FindByTagIDの実装

--- pkg/domain/repository/question.go
+++ pkg/domain/repository/question.go
@@ -5,5 +5,6 @@ import "github.com/traP-jp/h23w_10/pkg/domain"
 type QuestionRepository interface {
 	Find(limit, offset int) ([]domain.Question, error)
 	FindByID(id string) (*domain.Question, error)
+	FindByTagID(tagID string, limit, offset int) ([]domain.Question, error)
 	Create(question *domain.Question) (*domain.Question, error)
 }
