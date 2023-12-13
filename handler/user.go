66b6f68d68a503d7fa85efa29775d323e2a93b80
pirosiki197 <lingmukangtai825@gmail.com>
2023-12-13 23:08:58 +0900

    use string for url

--- handler/user.go
+++ handler/user.go
@@ -1,31 +1,34 @@
 package handler
 
 import (
+	"errors"
 	"net/http"
-	"net/url"
 
 	"github.com/labstack/echo/v4"
 	"github.com/traP-jp/h23w_10/pkg/domain"
+	"github.com/traP-jp/h23w_10/pkg/domain/repository"
 )
 
 type GetUserByIDResponse struct {
 	ID       string          `json:"id,omitempty"`
 	Name     string          `json:"name,omitempty"`
-	IconURL  url.URL         `json:"icon_url,omitempty"`
+	IconURL  string          `json:"icon_url,omitempty"`
 	UserType domain.UserType `json:"user_type,omitempty"`
 }
 
 func (h *Handler) GetUserByID(c echo.Context) error {
 	id := c.Param("id")
 	user, err := h.urepo.FindUserByID(id)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	if errors.Is(err, repository.ErrNotFound) {
+		return c.JSON(http.StatusNotFound, err)
+	} else if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
 	}
 
 	response := GetUserByIDResponse{
 		ID:       user.ID,
 		Name:     user.Name,
-		IconURL:  user.IconURL,
+		IconURL:  user.IconURL.String(),
 		UserType: user.UserType,
 	}
 	return c.JSON(http.StatusOK, response)
