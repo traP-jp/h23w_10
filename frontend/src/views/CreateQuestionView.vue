3900a0e5eed0b9fdc0258dee2265481fbb5549e6
mathsuky <yuida2718@gmail.com>
2023-12-14 22:50:39 +0900

    レビューいただいた内容について修正

--- frontend/src/views/CreateQuestionView.vue
+++ frontend/src/views/CreateQuestionView.vue
@@ -6,20 +6,22 @@
         <v-text-field
           label="タイトル"
           placeholder="質問のタイトルを入力"
-          v-model="title"
+          v-model="form.title"
           required
           class="ma-3"
         ></v-text-field>
-        <div class="mx-3"><MdEditor v-model="newQuestionContent" :language="language" /></div>
+        <div class="mx-3"><MdEditor v-model="form.content" :language="language" /></div>
         <v-autocomplete
           label="質問のタグを選択"
           :items="tags"
           item-title="name"
           item-value="id"
           class="mx-3 mt-6"
-          v-model="selectedTags"
+          v-model="form.tags"
           clearable
           multiple
+          chips
+          no-data-text="タグが見つかりません"
         />
         <div class="text-end mb-3 mr-3">
           <v-tooltip location="bottom" :disabled="canpostQuestion">
@@ -45,39 +47,35 @@
 <script setup lang="ts">
 import { MdEditor } from 'md-editor-v3'
 import 'md-editor-v3/lib/style.css'
-import { ref, computed } from 'vue'
+import { computed, reactive } from 'vue'
 import { postQuestion } from '@/lib/api/questions'
 import { getTags, type Tag } from '@/lib/api/tags'
 import { useRouter } from 'vue-router'
 
 const language = 'en-US'
-const title = ref('')
-const newQuestionContent = ref('# 質問内容を入力')
-const selectedTags = ref<Tag[]>([])
+const form = reactive<{ title: string; content: string; tags: Tag[] }>({
+  title: '',
+  content: '# 質問内容を入力',
+  tags: []
+})
 const tags = await getTags()
 const router = useRouter()
 
 const canpostQuestion = computed(() => {
-  return title.value.length > 0 && newQuestionContent.value.length > 0
+  return form.title.length > 0 && form.content.length > 0
 })
-const cleanUp = () => {
-  title.value = ''
-  newQuestionContent.value = '# 質問内容を入力'
-  selectedTags.value = []
-}
 
 const postNewQuestion = async () => {
-  const selectedTagIds: Omit<Tag, 'name'>[] = selectedTags.value.map((tag) => ({ id: tag.id }))
+  const selectedTagIds: Omit<Tag, 'name'>[] = form.tags.map((tag) => ({ id: tag.id }))
   try {
-    await postQuestion({
+    const res = await postQuestion({
       userId: 'masky', //TODO： ログインユーザーのIDを取得する
-      title: title.value,
-      content: newQuestionContent.value,
+      title: form.title,
+      content: form.content,
       tags: selectedTagIds,
       status: 'open'
     })
-    cleanUp()
-    router.push('/')
+    router.push(`/questions/${res.id}`)
   } catch (err) {
     console.log(err)
   }
