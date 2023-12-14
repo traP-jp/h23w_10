a705acdcab6689743f19177489fbf074c7af2b0b
pirosiki197 <lingmukangtai825@gmail.com>
2023-12-14 10:17:07 +0900

    getparams, callbackの実装

--- go.mod
+++ go.mod
@@ -11,10 +11,13 @@ require (
 	github.com/labstack/echo-contrib v0.15.0
 	github.com/labstack/echo/v4 v4.11.3
 	github.com/samber/lo v1.39.0
+	github.com/traPtitech/go-traq-oauth2 v0.0.0-20231128211850-d34d8c287ff9
+	golang.org/x/oauth2 v0.15.0
 )
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/gorilla/context v1.1.2 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
@@ -31,4 +34,6 @@ require (
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- handler/handler.go
+++ handler/handler.go
@@ -1,17 +1,36 @@
 package handler
 
-import "github.com/traP-jp/h23w_10/pkg/domain/repository"
+import (
+	"encoding/gob"
+
+	"github.com/traP-jp/h23w_10/pkg/domain/repository"
+	"golang.org/x/oauth2"
+)
 
 type Handler struct {
 	qrepo repository.QuestionRepository
 	arepo repository.AnswerRepository
 	urepo repository.UserRepository
+
+	oauth2Config oauth2.Config
 }
 
-func NewHandler(qrepo repository.QuestionRepository, arepo repository.AnswerRepository, urepo repository.UserRepository) *Handler {
+func NewHandler(
+	qrepo repository.QuestionRepository,
+	arepo repository.AnswerRepository,
+	urepo repository.UserRepository,
+	oauth2Conf oauth2.Config,
+) *Handler {
+	gob.Register(&oauth2.Token{})
 	return &Handler{
 		qrepo: qrepo,
 		arepo: arepo,
 		urepo: urepo,
+
+		oauth2Config: oauth2Conf,
 	}
 }
+
+const (
+	sessionName = "h23w_10"
+)
