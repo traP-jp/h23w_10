eab5a2fb7521bdabed4eb8f6ca11695799cde569
pirosiki197 <lingmukangtai825@gmail.com>
2023-12-15 12:02:57 +0900

    add auth middleware

---
+++ handler/middleware.go
@@ -0,0 +1,23 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+)
+
+func UserAuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		sess, err := session.Get(sessionName, c)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+		userID, ok := sess.Values["userID"].(string)
+		if !ok {
+			next(c)
+		}
+		c.Set("userID", userID)
+		return next(c)
+	}
+}
