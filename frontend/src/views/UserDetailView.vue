352ae2dab4b6a1ef954fab1e3efc9b55ffd842a6
mathsuky <yuida2718@gmail.com>
2023-12-16 19:41:08 +0900

    User型の修正

--- frontend/src/views/UserDetailView.vue
+++ frontend/src/views/UserDetailView.vue
@@ -3,7 +3,7 @@
     <v-row>
       <v-col cols="4">
         <h1>User Name: {{ userInfo?.name }}</h1>
-        <img :src="userInfo?.iconURL" alt="User Icon" />
+        <img :src="userInfo?.icon_url" alt="User Icon" />
       </v-col>
       <v-col cols="8">
         <h1>円形に表示するやつ</h1>
@@ -29,8 +29,8 @@ onMounted(async () => {
     const res = await getUser(request)
     userInfo.value = res
     userName.value = res.name
-    console.log(userInfo.value.name)
-    console.log(userInfo.value.iconURL)
+    console.log(res)
+    console.log(userInfo)
   } catch (err) {
     console.error(err)
   }
