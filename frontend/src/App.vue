0f2a3333917de195a5cb7b435e375e11a18939d9
mathsuky <yuida2718@gmail.com>
2023-12-16 13:15:45 +0900

    ログイン機能の実装

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,6 +9,7 @@
       <v-app-bar-nav-icon @click="toggleDrawer" />
       <v-app-bar-title>staQoverflow</v-app-bar-title>
       <!-- todo: add user icon / login button -->
+      <v-btn color="primary" @click="handleLogin">ログイン</v-btn>
     </v-app-bar>
     <v-main>
       <Suspense>
@@ -21,9 +22,20 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import { ref } from 'vue'
-
+import { BASE } from '@/lib/api/index'
 const showDrawer = ref(false)
 const toggleDrawer = () => (showDrawer.value = !showDrawer.value)
+const handleLogin = async () => {
+  try {
+    const res = await fetch(`${BASE}/oauth2/params`, {})
+    const data = await res.json()
+    console.log(data)
+    window.location.href = data.url
+  } catch (error) {
+    console.error(error)
+  }
+  // ここにログイン処理を書く
+}
 </script>
 
 <style scoped></style>
