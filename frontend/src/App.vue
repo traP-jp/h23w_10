d8beba0a8d2a65221390b06ff52f2bccd3680766
mathsuky <yuida2718@gmail.com>
2023-12-16 15:50:00 +0900

    user情報を取得できるように

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,8 +8,14 @@
     <v-app-bar>
       <v-app-bar-nav-icon @click="toggleDrawer" />
       <v-app-bar-title>staQoverflow</v-app-bar-title>
-      <!-- todo: add user icon / login button -->
-      <v-btn color="primary" @click="handleLogin">ログイン</v-btn>
+      <template v-if="isLoggedIn">
+        <v-icon>mdi-account</v-icon>
+        <p>yu-za-na-me</p>
+        <v-btn color="primary" @click="showUserInfo">ユーザー情報</v-btn>
+      </template>
+      <template v-else>
+        <v-btn color="primary" @click="handleLogin">ログイン</v-btn>
+      </template>
     </v-app-bar>
     <v-main>
       <Suspense>
@@ -21,21 +27,42 @@
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { BASE } from '@/lib/api/index'
+import { useRouter } from 'vue-router'
+
 const showDrawer = ref(false)
 const toggleDrawer = () => (showDrawer.value = !showDrawer.value)
 const handleLogin = async () => {
   try {
     const res = await fetch(`${BASE}/oauth2/params`, {})
     const data = await res.json()
-    console.log(data)
     window.location.href = data.url
   } catch (error) {
     console.error(error)
   }
-  // ここにログイン処理を書く
 }
+
+const router = useRouter()
+const showUserInfo = async () => {
+  try {
+    const res = await fetch(`${BASE}/users/me`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('access_token')}`
+      }
+    })
+    if (res.status === 401) {
+      window.alert('ログイン画面に遷移します')
+      handleLogin()
+      return
+    }
+    const data = await res.json()
+    router.push(`/users/${data.id}`)
+  } catch (error) {
+    console.error(error)
+  }
+}
+const isLoggedIn = computed(() => localStorage.getItem('access_token'))
 </script>
 
 <style scoped></style>
