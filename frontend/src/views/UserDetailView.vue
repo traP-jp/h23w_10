49ad0fabad03e48510b4e4bc754ec545eff13de7
mathsuky <yuida2718@gmail.com>
2023-12-17 13:44:22 +0900

    ユーザー情報の表示

--- frontend/src/views/UserDetailView.vue
+++ frontend/src/views/UserDetailView.vue
@@ -1,15 +1,7 @@
 <template>
   <div style="width: 80%; margin: auto">
-    <v-row>
-      <v-col cols="4">
-        <h1>User Name: {{ userInfo?.name }}</h1>
-        <img :src="userInfo?.icon_url" alt="User Icon" />
-      </v-col>
-      <v-col cols="8">
-        <h1>円形に表示するやつ</h1>
-        <img :src="userImage ?? ''" alt="show Circle Image" />
-      </v-col>
-    </v-row>
+    <h1>{{ userInfo?.name }} さんのページ</h1>
+    <img :src="`${BASE}/imggen?user_id=${userInfo?.id}`" alt="User Image" />
   </div>
 </template>
 
@@ -22,7 +14,6 @@ import { useRoute } from 'vue-router'
 const route = useRoute()
 const userInfo = ref<User>()
 const userName = ref('')
-const userImage = ref<string | null>(null)
 const request: GetUserRequest = {
   id: route.params.id as string
 }
@@ -32,14 +23,6 @@ onMounted(async () => {
     const res = await getUser(request)
     userInfo.value = res
     userName.value = res.name
-    const res2 = await fetch(`${BASE}/imggen`, {
-      method: 'POST',
-      body: JSON.stringify({
-        // user_id: '1eea935c-0d3c-411b-a565-1b09565237f4'
-        user_id: userInfo.value.id
-      })
-    })
-    console.log(res2)
   } catch (err) {
     console.error(err)
   }
