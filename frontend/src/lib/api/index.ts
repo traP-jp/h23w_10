d370e02701648ba257350932ec6a28f16b369358
detteiu8383 <>
2023-12-17 11:42:12 +0900

    :sparkles: add configs for local dev env

--- frontend/src/lib/api/index.ts
+++ frontend/src/lib/api/index.ts
@@ -1,5 +1,5 @@
 // api使用時のurlのベース
-export const BASE = 'https://staqoverflow.trap.show/api'
+export const BASE = import.meta.env.DEV ? "http://localhost:8000" : 'https://staqoverflow.trap.show/api'
 
 export const useMock = false
 // export const useMock = import.meta.env.DEV
