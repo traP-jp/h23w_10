28b851d17e61488cce09f9ce1557a5c2a3b33b87
detteiu8383 <>
2023-12-17 10:59:13 +0900

    :adhesive_bandage: fix questions view

--- frontend/src/components/DetailCard.vue
+++ frontend/src/components/DetailCard.vue
@@ -3,15 +3,10 @@
     <MdPreview :editorId="props.editorId" :modelValue="props.content" />
     <v-card-actions class="d-flex justify-space-between">
       <div class="d-flex">
-        <v-btn :to="`$/users/${props.userId}`">
+        <v-btn :to="`$/users/${props.user}`">
           <v-avatar size="small" class="avatar">
             <div class="mr--1"></div>
-            <v-img
-              :src="user?.icon_url ?? ''"
-              aspect-ratio="1"
-              contain
-              class="avatar-image"
-            ></v-img>
+            <v-img :src="user?.icon_url ?? ''" aspect-ratio="1" contain class="avatar-image"></v-img>
           </v-avatar>
           <div style="margin-left: -8px">
             <v-chip variant="text" color="grey">{{ user?.name }}</v-chip>
@@ -19,12 +14,11 @@
         </v-btn>
         <div style="margin-top: 3px">
           <v-chip variant="text" color="grey">
-            {{ diffHuman(props.createdAt ?? new Date()).diff }}</v-chip
-          >
+            {{ diffHuman(props.createdAt ?? new Date()).diff }}</v-chip>
         </div>
       </div>
 
-      <div v-if="loginUser?.id === props.userId">
+      <div v-if="loginUser?.id === props.user.id">
         <v-btn @click="showModal(props.content, isQuestion)">編集</v-btn>
       </div>
     </v-card-actions>
@@ -34,30 +28,18 @@
 <script setup lang="ts">
 import { MdPreview } from 'md-editor-v3'
 import { diffHuman } from '@/lib/format'
-import { ref, onMounted } from 'vue'
-import { getUser, type User } from '@/lib/api/users'
+import { type User } from '@/lib/api/users'
 import { inject, type Ref } from 'vue'
 
 const loginUser = inject<Ref<User | null>>('loginUser')
-const user = ref<User>()
 export interface Props {
   editorId: string
   content: string
-  userId: string
+  user: User
   createdAt: Date | undefined
   showModal: Function
   isQuestion: boolean
 }
 
 const props = defineProps<Props>()
-
-onMounted(async () => {
-  try {
-    const res = await getUser({ id: props.userId }) // 本番
-    // const res = await getUser({ id: 'test-user-0' }) // テスト
-    user.value = res
-  } catch (err) {
-    console.error(err)
-  }
-})
 </script>
